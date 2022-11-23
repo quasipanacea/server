bca099edb6fc6d3695267b6cb97012e98be809bc
Edwin Kofler <edwin@kofler.dev>
2022-11-23 03:34:33 -0800

    chore: Update deps

--- src/mod.ts
+++ src/mod.ts
@@ -1,14 +1,14 @@
-export * as path from "https://deno.land/std@0.152.0/path/mod.ts";
-export * as fs from "https://deno.land/std@0.152.0/fs/mod.ts";
-export * as conversion from "https://deno.land/std@0.152.0/streams/conversion.ts";
-export { Status } from "https://deno.land/std@0.152.0/http/http_status.ts";
+export * as path from "https://deno.land/std@0.165.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.165.0/fs/mod.ts";
+export * as conversion from "https://deno.land/std@0.165.0/streams/conversion.ts";
+export { Status } from "https://deno.land/std@0.165.0/http/http_status.ts";
 
-export { z } from "https://deno.land/x/zod@v3.18.0/mod.ts";
+export { z } from "https://deno.land/x/zod@v3.19.1/mod.ts";
 export {
 	Application,
 	Router,
 	Context,
 	send,
-} from "https://deno.land/x/oak@v10.6.0/mod.ts";
-export { helpers } from "https://deno.land/x/oak@v10.6.0/mod.ts";
-export { type Middleware } from "https://deno.land/x/oak@v10.6.0/middleware.ts";
+} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+export { helpers } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+export { type Middleware } from "https://deno.land/x/oak@v11.1.0/middleware.ts";
